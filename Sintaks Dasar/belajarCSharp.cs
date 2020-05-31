0f29096f3c3595044e517f69867df35a9c7e64fd
ibnuahmadfauzi <59650987+ibnuahmadfauzi@users.noreply.github.com>
2020-05-31 18:33:44 +0700

    Membuat belajarCSharp.cs
    31-06-2020

---
+++ Sintaks Dasar/belajarCSharp.cs
@@ -0,0 +1,17 @@
+// File Library
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace belajarCShrap // Untuk menampung class-class
+{
+    class Program // Untuk menampung fungsi-fungsi
+    {
+        static void Main(string[] args) // Main method
+        {
+            Console.WriteLine("Hello World!"); // Menampilkan output teks Hello World! di console
+            Console.ReadKey(); // Agar program membuat jeda sebelum program tertutup
+        }
+    }
+}
